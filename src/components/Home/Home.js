f4592a8bc25267136b040a66134a26f0255000ec
Lakrafli ismail <lakraflii.ismail@gmail.com>
2024-06-06 21:49:31 +0200

    Forcer la reconstruction de GitHub Pages

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -22,7 +22,7 @@ function Home() {
 
               <h1 className="heading-name">
                 I'M
-                <strong className="main-name"> Lakrafli  Ismail</strong>
+                <strong className="main-name"> Lakrafli  ismail</strong>
               </h1>
 
               <div style={{ padding: 50, textAlign: "left" }}>
